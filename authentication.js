8fd6ad3612bab0f0a95c19eea738d5434e11e975
Akhil-code-heisenbug <114263685+Akhil-code-heisenbug@users.noreply.github.com>
2024-05-01 12:22:43 +0530

    Add files via upload

---
+++ authentication.js
@@ -0,0 +1,144 @@
+/**
+  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
+  - Don't need to use any database to store the data.
+
+  - Save the users and their signup/login data in an array in a variable
+  - You can store the passwords in plain text (as is) in the variable for now
+
+  The expected API endpoints are defined below,
+  Testing the server - run `npm run test-authenticationServer` command in terminal
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs= require('fs');
+const Port = 3000
+const app = express();
+var users=[];
+const path ="C:\\Users\\akhil\\Desktop\\HTML TUT\\todoapp\\users.json"
+app.use(bodyParser.json());
+
+//  1. POST /signup - User Signup
+//  Description: Allows users to create an account. This should be stored in an array on a file , and a unique id should be generated for every new user that is added.
+//  Request Body: JSON object with username, password, firstName and lastName fields.
+//  Response: 201 Created if successful, or 400 Bad Request if the username already exists.
+//  Example: POST http://localhost:3000/signup
+
+app.post('/signup', (req, res) => {
+  const newuser = {
+      email: req.body.email,
+      password: req.body.password
+  };
+
+  // Perform validation on new user data
+  if (!newuser.email || !newuser.password) {
+      return res.status(400).send("Email and password are required");
+  }
+
+  fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+          console.error("Error reading user data:", err);
+          return res.sendStatus(500);
+      }
+
+      const users = JSON.parse(data);
+      const userAlreadyExists = users.some(user => user.email === newuser.email);
+
+      if (userAlreadyExists) {
+          return res.status(409).send("User already exists");
+      }
+
+      users.push(newuser);
+
+      fs.writeFile(path,JSON.stringify(users), (err) => {
+          if (err) {
+              console.error("Error writing user data:", err);
+              return res.sendStatus(500);
+          }
+
+          console.log("User added:", newuser);
+          return res.status(201).send("Signup successful");
+      });
+  });
+});
+
+
+
+// 2. POST /login - User Login
+// Description: Gets user back their details like firstname, lastname and id
+// Request Body: JSON object with username and password fields.
+// Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
+// Example: POST http://localhost:3000/login
+app.post("/login",(req,res)=>{
+fs.readFile("authentication.json","utf-8",(err,data)=>{
+  if(err) throw err;
+  var user = req.body
+  var userFound = null;
+  for(var i = 0;i<users.length;i++){
+   if(users[i].username ===user.username&&users[i].password === user.password){
+    userFound = user[i];
+    break;
+   }
+  }
+  if(userFound){
+    res.json({
+      firstname:userFound.firstname,
+      lastname: userFound.lastname,
+      email:user[i].email,
+    });
+  }
+else{
+  res.status(400).send("user Not Found");
+}
+});
+})
+
+
+
+// 3. GET /data - Fetch all user's names and ids from the server (Protected route)
+// Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
+// The users username and password should be fetched from the headers and checked before the array is returned
+// Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+// Example: GET http://localhost:3000/data
+
+// - For any other route not defined in the server return 404
+app.get("/data",(req,res)=>{
+  fs.readFile(path,"utf-8",(err,data)=>{
+    if(err) throw err;
+    var email = req.header.email;
+    var password= req.header.password;
+    var userFound = true;
+    for (var i = 0; i<users.length; i++) {
+  
+      if (users[i].email === email && users[i].password === password) {
+          userFound = true;
+          break;
+      }
+    }
+  
+    if (userFound){
+      let usersToReturn = [];
+      for (let i = 0; i<users.length; i++) {
+          usersToReturn.push({
+              firstName: users[i].firstName,
+              lastName: users[i].lastName,
+              email: users[i].email
+          });
+      }
+      res.json({
+          users:usersToReturn
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  });
+  })
+  
+//------------------------------
+app.get('/show',(req,res)=>{
+  fs.readFile(path,"utf-8",(err,data)=>{
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(Port)
